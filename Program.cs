f8ef6082da61a54d7a68b79a395d4b8880fef432
slompy <krakenov.slava@mail.ru>
2026-09-20 16:41:16 +0300

    Lab01: variables and interpolation

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string myName = "Даниил";
+string myGroupName = "ИСП-252";
+int myCourseNumber = 2;
+double myavarageGrade = 4.5;
+bool isIStudent = true;
+
+// Console.WriteLine($"Студент: {myName}");
+// Console.WriteLine($"Группа: {myGroupName}");
+// Console.WriteLine($"Курс: {myCourseNumber}");
+// Console.WriteLine($"Средний балл: {myavarageGrade}");
+// Console.WriteLine($"Студент ли я: {isIStudent}");
+
+Console.WriteLine($"Студент: {myName}, Группа: {myGroupName} Курс: {myCourseNumber} Средний балл: {myavarageGrade} Студент ли я: {isIStudent}");Console.WriteLine("Hello, World!");
